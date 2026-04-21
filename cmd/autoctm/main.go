c28f88f3006f9ff30c61cd23337ec6beb728ef46
Oscar <ashburosca@myvuw.ac.nz>
2026-04-22 09:21:25 +1200

    feature/cli: Added instace init condition.

--- cmd/autoctm/main.go
+++ cmd/autoctm/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"flag"
+)
+
+func main() {
+	initFlag := flag.Bool("init", false, "Initialize first instance")
+
+	flag.Parse()
+
+	if *initFlag {
+		// TODO: Run instace initialization verification.
+		// TODO: If no instace, initilize intance.
+	}
+
+}
