e3c77bfb00f2bf01f8452b3b75576fc025f992dd
Oscar Ashburn <ashburn.oscar@gmail.com>
2026-05-19 01:51:50 +1200

    feature/broker: Implement Start Instance on the broker.
    When start Instance is called, a uuid is generated and starts an autoctm-instance.

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"sorcerer.nz/autoctm/internal/instance"
+	"github.com/google/uuid"
 )
 
 type Broker interface {
@@ -17,41 +18,76 @@ type Broker interface {
 	GetInstance(id string) (*instance.Instance, error)
 }
 
+type instanceBroker struct {
+    mu        sync.Mutex
+    instances map[string]*managedInstance
+}
+
+type  managedInstance struct {
+	id string
+	process *os.process
+	conn net.Conn
+	status string
+}
+
+
 
 func New() Broker {
-	return &simpleBroker{}
+	return &instanceBroker{}
 }
 
-//TODO Implement stubs later when cli is finished
-func (b *simpleBroker) StartInstance() (string, error) {
-	fmt.Println("[broker] StartInstance called")
-	return "inst-1", nil
+func (b *instanceBroker) StartInstance() (string, error) {
+    // Generate a UUID for the instance (will become the DB primary key later)
+    id := uuid.New().String()
+
+    // Spawn autoctm-instance as a child process, passing only the ID per spec 3.2.3
+    cmd := exec.Command("autoctm-instance", id)
+    if err := cmd.Start(); err != nil {
+        return "", fmt.Errorf("failed to spawn instance: %w", err)
+    }
+
+    b.mu.Lock()
+    b.instances[id] = &managedInstance{
+        id:      id,
+        process: cmd.Process,
+        status:  "running",
+    }
+    b.mu.Unlock()
+
+    // Wait for the process to exit in the background.
+
+	// TODO: Implement in IPC
+	// Dispatch the Grim Reaper on process to avoid Zombies.
+	go func() { cmd.Wait() }()
+
+    fmt.Printf("[broker] started instance %s (pid %d)\n", id, cmd.Process.Pid)
+    return id, nil
 }
 
-func (b *simpleBroker) StopInstance(id string) error {
+func (b *instanceBroker) StopInstance(id string) error {
 	return nil
 }
 
-func (b *simpleBroker) Pause(id string) error {
+func (b *instanceBroker) Pause(id string) error {
 	return nil
 }
 
-func (b *simpleBroker) Resume(id string) error {
+func (b *instanceBroker) Resume(id string) error {
 	return nil
 }
 
-func (b *simpleBroker) AddLog(instanceID, url string) error {
+func (b *instanceBroker) AddLog(instanceID, url string) error {
 	return nil
 }
 
-func (b *simpleBroker) RemoveLog(instanceID, url string) error {
+func (b *instanceBroker) RemoveLog(instanceID, url string) error {
 	return nil
 }
 
-func (b *simpleBroker) ListInstances() ([]*instance.Instance, error) {
+func (b *instanceBroker) ListInstances() ([]*instance.Instance, error) {
 	return nil, nil
 }
 
-func (b *simpleBroker) GetInstance(id string) (*instance.Instance, error) {
+func (b *instanceBroker) GetInstance(id string) (*instance.Instance, error) {
 	return nil, nil
 }
