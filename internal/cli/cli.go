a3560a5600a76c72e4767fe2307a9f93f8c8450a
Oscar Ashburn <ashburn.oscar@gmail.com>
2026-05-19 00:26:23 +1200

    feature/broker: cli now uses protocol package.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,71 +1,65 @@
 package cli
 
 import (
-	"time"
-	"net"
+	"encoding/binary"
 	"encoding/json"
-    "encoding/binary"
-    "io"
-    "sorcerer.nz/autoctm/internal/protocol"
+	"io"
+	"net"
+	"time"
+
+	"sorcerer.nz/autoctm/internal/protocol"
 )
 
-func (c *CLI) recv() (Response, error) {
-    // Read length prefix
-    var length uint32
-    if err := binary.Read(c.conn, binary.BigEndian, &length); err != nil {
-        return Response{}, err
-    }
+func (c *CLI) recv() (protocol.Response, error) {
+    // Read length prefix 
+	var length uint32
+	if err := binary.Read(c.conn, binary.BigEndian, &length); err != nil {
+		return protocol.Response{}, err
+	}
+    // Read the body 
+	buf := make([]byte, length)
+	if _, err := io.ReadFull(c.conn, buf); err != nil {
+		return protocol.Response{}, err
+	}
 
-    // Read the body
-    buf := make([]byte, length)
-    if _, err := io.ReadFull(c.conn, buf); err != nil {
-        return Response{}, err
-    }
+	var resp protocol.Response
+	if err := json.Unmarshal(buf, &resp); err != nil {
+		return protocol.Response{}, err
+	}
 
-    // Unmarshal into Response
-    var resp Response
-    if err := json.Unmarshal(buf, &resp); err != nil {
-        return Response{}, err
-    }
-
-    return resp, nil
+	return resp, nil
 }
 
-// Send a command.
 func (c *CLI) send(cmd string, args any) error {
-
-	//Encode command and args.
-	command := Command{Cmd:cmd}
+	command := protocol.Command{Cmd: cmd}
 
 	if args != nil {
 		encodedArgs, err := json.Marshal(args)
 		if err != nil {
-            return err
-        }
+			return err
+		}
 		command.Args = encodedArgs
 	}
-	data,err := json.Marshal(command)
 
-    if err != nil {
-        return err
-    }
+	data, err := json.Marshal(command)
+	if err != nil {
+		return err
+	}
 
-	//Send command over socket.
-    length := uint32(len(data))
-    if err := binary.Write(c.conn, binary.BigEndian, length); err != nil {
-        return err
-    }
+	length := uint32(len(data))
+	if err := binary.Write(c.conn, binary.BigEndian, length); err != nil {
+		return err
+	}
 
-    _, err = c.conn.Write(data)
-    return err
+	_, err = c.conn.Write(data)
+	return err
 }
 
-func (c *CLI) sendAndWait(cmd string, args any) (Response, error) {
-    if err := c.send(cmd, args); err != nil {
-        return Response{}, err
-    }
-
-    return c.recv()
+func (c *CLI) sendAndWait(cmd string, args any) (protocol.Response, error) {
+	if err := c.send(cmd, args); err != nil {
+		return protocol.Response{}, err
+	}
+	return c.recv()
 }
 
 type InstanceContext struct {
@@ -76,13 +70,12 @@ type InstanceContext struct {
 }
 
 type CLI struct {
-    conn    net.Conn
-    context *InstanceContext
+	conn    net.Conn
+	context *InstanceContext
 }
 
-
 func New(conn net.Conn) *CLI {
-    return &CLI{
-        conn: conn,
-    }
+	return &CLI{
+		conn: conn,
+	}
 }
